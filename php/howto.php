650a18b184ea3220f84faa282e67af11a46abadb
Caryn12 <87209562+Caryn12@users.noreply.github.com>
2022-01-17 01:27:49 +0700

    Create howto.php

---
+++ php/howto.php
@@ -0,0 +1,31 @@
+<?php
+$identifier = 1;
+include("page_header.php");
+include("../config.php");
+
+?>
+
+
+
+<div class="main-container">
+    <section class="s2 text-center">
+        <div class="container">
+            <div class="hero-picture">
+                <img src="../img/pic5.jpg">
+            </div>
+            <!-- Content here -->
+            <div class="hero-word">
+                <h1>Website Supporting Donation</h1>
+                <p>Register with WearIt application through our online platform for absolutely free. Create your brand profile for clothing recycling or donation. Get started – IT'S SUPER EASY.</p>
+                <div id="learnmore-button">
+                    <a href="../php/signup.php">GET STARTED</a>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+
+</div>
+
+
+<?php include("page_footer.php"); ?>
